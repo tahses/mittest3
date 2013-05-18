60cebd8ce2a0f510784aa452119fab9757ffd0d2
asfaw <asfaw@thetahses.com>
2013-05-18 02:16:11 -0400

    df1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mittest::Application.config.secret_key_base = 'c47131f332e2e5e30afc8176229d7574b58601a0fa0b38c24c4225c1b72b70c1e5b60f25b71ce67c2d5ec456cfa8f0f05f44f36f586e2e3e8103fc716e967d8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mittest::Application.config.session_store :cookie_store, key: '_Mittest_session'
